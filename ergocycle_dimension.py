cae8f17769407fdf8ef922fada7f52c94130947d
Benoît Sinivassin <benoit.sinivassin@laas.fr>
2015-04-17 19:08:03 +0200

    3rd commit ergocycle VRML (stable) + python script (stable)

--- ergocycle_dimension.py
+++ ergocycle_dimension.py
@@ -29,29 +29,29 @@ def main():
 	i=0
 	new_file=open('ergocycle_temp.txt','w')
 	old_file=open('ergocycle_main.wrl','r')
-	while i<275:
-		for i in range(0,256):
+	while i<386:
+		for i in range(0,308):
 			i=i+1
 			line=old_file.readline()
 			new_file.write(str(line))
 		line=old_file.readline()
 		string='        translation '+str(-height_saddle/2.2)+' 0.0 '+str(height_saddle)+' #modif origin of saddle'+'\n'
 		new_file.write(string)
-		for i in range(257,275):
+		for i in range(309,327):
 			i=i+1
 			line=old_file.readline()
 			new_file.write(str(line))
 		line=old_file.readline()
 		string='        translation '+str(height_handlebar/4.3)+' 0.0 '+str(height_handlebar)+' #modif origin of supp_handlebar'+'\n'
 		new_file.write(string)
-		for i in range(276,291):
+		for i in range(328,344):
 			i=i+1
 			line=old_file.readline()
 			new_file.write(str(line))
 		line=old_file.readline()
-		string='        translation '+str(lenght_handlebar)+' 0.0 0.0 #modif origin of handlebar (x=[0.0,0.3])'+'\n'
+		string='		translation '+str(lenght_handlebar)+' 0.0 0.0 #modif origin of handlebar (x=[0.0,0.3])'+'\n'
 		new_file.write(string)
-		for i in range(292,332):
+		for i in range(345,386):
 			i=i+1
 			line=old_file.readline()
 			new_file.write(str(line))
